79cb2097734c0fb6e48c096b3ef110bb17185600
Guillaume Hostache <guillaume.hostache_pro@outlook.fr>
2023-11-12 09:31:24 +0100

    ajout structure (base) + gestion fichier Json entree

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Projet_Agile_M1I
+## Conception agile de projets informatiques
+
+### Description
+
+Université Lumière Lyon 2 - Campus "Portes des Alpes"
+
+Cours de M1 Informatique - Enseignant : M. Valentin Lachand-Pascal
+
+### Utilisation
+
+...
---
+++ styles/script.js
@@ -0,0 +1,32 @@
+function chargerFichierJson(evt) {
+    console.log("chargerFichierJson");
+
+    const fichierJson = JSON.parse(evt.target.result);
+    console.log(fichierJson);
+}
+
+function selectionnerFichierJson() {
+    console.log("chargerFichier");
+    const saisie = document.getElementById('jsonFile');
+    
+    // Vérifie si un fichier a été sélectionné
+    if (saisie.files.length > 0) {
+        const fichier = saisie.files[0];
+
+        // Vérifie si le fichier est au format JSON
+        if (fichier.type === 'application/json') {
+            // Lecture du fichier JSON
+            const lecteur = new FileReader();
+
+            lecteur.onload = chargerFichierJson;
+
+            lecteur.readAsText(fichier);
+        } else {
+            alert('Veuillez sélectionner un fichier JSON valide.');
+        }
+    } else {
+        alert('Veuillez sélectionner un fichier.');
+    }
+}
+
+window.onload;
